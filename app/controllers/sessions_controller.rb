41dec586c1eff34eeb4c64abe9aaa0f30c8048c9
Shuyawa89 <shuyaiwamoto@gmail.com>
2023-08-08 17:37:35 +0900

    update session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #ユーザページにリダイレクトする
+    else
+      #エラーメッセージの表示
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destory
+
+  end
 end
