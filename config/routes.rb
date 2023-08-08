337010c8fbe7cff6b6cf4d8934b0dc4a8ca2e849
Shuyawa89 <shuyaiwamoto@gmail.com>
2023-08-08 16:48:13 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  root "static_pages#home"
-  get  "/help",    to: "static_pages#help"
-  get  "/about",   to: "static_pages#about"
-  get  "/contact", to: "static_pages#contact"
-  get  "/signup",  to: "users#new"
+  get   'sessions/new'
+  root  "static_pages#home"
+  get   "/help",    to: "static_pages#help"
+  get   "/about",   to: "static_pages#about"
+  get   "/contact", to: "static_pages#contact"
+  get   "/signup",  to: "users#new"
+  get   "/login",   to: "sessions#new"
+  get   "/login",   to: "sessions#create"
+  post  "/logout",   to: "sessions#destory"
   resources :users #ユーザー情報取得のさまざまなメソッドなどを使えるようにする
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
