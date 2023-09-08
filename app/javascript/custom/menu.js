b944379b711175651f59a203ef76cae9120fdb70
Shuyawa89 <shuyaiwamoto@gmail.com>
2023-09-08 18:12:07 +0900

    debug applicationjs

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -4,8 +4,18 @@ document.addEventListener("turbo:load",function(){
     let account = document.querySelector("#account");
     account.addEventListener("click", function(event){
         event.preventDefault();
-        let menu = document.querySelector("$dropdown-menu");
+        let menu = document.querySelector("#dropdown-menu");
         menu.classList.toggle("active");
     });
 });
+
+//正解
+// document.addEventListener("turbo:load", function () {
+//     let account = document.querySelector("#account");
+//     account.addEventListener("click", function (event) {
+//         event.preventDefault();
+//         let menu = document.querySelector("#dropdown-menu");
+//         menu.classList.toggle("active");
+//     });
+// });
 
