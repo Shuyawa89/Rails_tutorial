6ce00db5c868591ec6c989e4de992e3b927eeec5
Shuyawa89 <shuyaiwamoto@gmail.com>
2023-09-08 19:19:05 +0900

    refactor menu.js

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,23 +1,30 @@
 //メニュー操作
-//トグルリスナーを追加して、クリックをリッスンする
-document.addEventListener("turbo:load",function(){
-    let hamburger = document.querySelector("#hamburger");
-    hamburger.addEventListener("click", function(event){
-        event.preventDefault();
-        let menu = document.querySelector("#navbar-menu");
-        menu.classList.toggle("collapse")
-    })
 
-    let account = document.querySelector("#account");
-    account.addEventListener("click", function(event){
+//トグルリスナーを追加する
+const addToggleListener = (selected_id, menu_id, toggle_class) => {
+    let selected_element = document.querySelector(`#${selected_id}`);
+    selected_element.addEventListener("click", (event) => {
         event.preventDefault();
-        let menu = document.querySelector("#dropdown-menu");
-        menu.classList.toggle("active");
+        let menu = document.querySelector(`#${menu_id}`);
+        menu.classList.toggle(toggle_class);
     });
+}
+
+//クリックをリッスンするトグルリスナーを追加する
+document.addEventListener("turbo:load", () => {
+    addToggleListener("hamburger", "navbar-menu", "collapse");
+    addToggleListener("account", "dropdown-menu", "active");
 });
 
-//正解
+//トグルリスナーを追加して、クリックをリッスンする
 // document.addEventListener("turbo:load", function () {
+//     let hamburger = document.querySelector("#hamburger");
+//     hamburger.addEventListener("click", function (event) {
+//         event.preventDefault();
+//         let menu = document.querySelector("#navbar-menu");
+//         menu.classList.toggle("collapse")
+//     })
+
 //     let account = document.querySelector("#account");
 //     account.addEventListener("click", function (event) {
 //         event.preventDefault();
@@ -25,4 +32,5 @@ document.addEventListener("turbo:load",function(){
 //         menu.classList.toggle("active");
 //     });
 // });
+
 
