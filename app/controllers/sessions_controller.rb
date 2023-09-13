0119c1c5c1323e6fb9bf520f419e97298f76418e
Shuyawa89 <shuyaiwamoto@gmail.com>
2023-09-13 14:54:32 +0900

    update test view cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
       #ユーザページにリダイレクトする
       reset_session #ログインの直前にこれを行う
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      log_in user
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      log_in @user
+      redirect_to @user
     else
       #エラーメッセージの表示
       flash.now[:danger] = 'Invalid email/password combination'
