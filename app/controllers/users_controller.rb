7e39e72a3dcfc8419431f9df2ca7becd5d9110d5
Shuyawa89 <shuyaiwamoto@gmail.com>
2023-09-17 14:58:03 +0900

    update create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session
-      log_in @user
-      #成功した場合の処理
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now #メールを送信する
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new', status: :unprocessable_entity     
     end
