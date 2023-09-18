2ba484e09cb155a882ca3be49a6d40b2c0fd0ba2
Shuyawa89 <shuyaiwamoto@gmail.com>
2023-09-19 02:31:26 +0900

    add delete link

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :corrent_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -7,6 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = current_user.feed.paginate(page: params[:page])
       render 'static_pages/home', status: :unprocessable_entity
     end
   end
@@ -18,5 +20,10 @@ class MicropostsController < ApplicationController
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url, status: :see_other if @micropost.nil?
     end
 end
