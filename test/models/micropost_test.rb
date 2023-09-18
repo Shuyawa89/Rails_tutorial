d250a0b15ddd2dd64c6e021f53b0e65b84040958
Shuyawa89 <shuyaiwamoto@gmail.com>
2023-09-18 22:59:42 +0900

    add associated microposts destroy

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -5,7 +5,9 @@ class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
     # このコードは慣習的に正しくない
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    #@micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    # ユーザ経由でマイクロポストを生成する(慣習的に正しい)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -25,5 +27,9 @@ class MicropostTest < ActiveSupport::TestCase
   test "content should be at most 140 characters" do
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first  # fixture(xxx.yml)がある前提
   end
 end
